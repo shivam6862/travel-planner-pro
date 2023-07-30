852478badea76c61e3cce645d92b81e0446939b0
Shubham Sharma <s_sharma2@me.iitr.ac.in>
2023-07-30 18:48:37 +0530

    map

--- Frontend/src/Components/Map.jsx
+++ Frontend/src/Components/Map.jsx
@@ -11,7 +11,7 @@ import {
 } from "react-leaflet";
 import { useMapEvents } from "react-leaflet";
 import RoutingMachine from "./RoutingControl";
-
+import { Box } from "@mui/material";
 const maps = {
   base: "https://api.maptiler.com/maps/basic/256/{z}/{x}/{y}.png?key=6HsWF1PPM2UmlGH3TpIe",
 };
@@ -43,11 +43,12 @@ const Map = ({ searchTerm }) => {
 
   return (
     <>
-      <MapContainer
+   
+    <MapContainer
         center={center}
         zoom={5}
         zoomControl={false}
-        style={{ height: "100vh", width: "100%", padding: 0 }}
+        style={{ height: "100vh", width: "90%", padding: 0 , borderRadius:'30px', boxShadow: 'rgba(50, 50, 93, 0.25) 0px 50px 100px -20px, rgba(0, 0, 0, 0.3) 0px 30px 60px -30px, rgba(10, 37, 64, 0.35) 0px -2px 6px 0px inset'}}
         whenCreated={(map) => setMap(map)}
         touchZoom={true}
       >
@@ -74,6 +75,8 @@ const Map = ({ searchTerm }) => {
           <Circle center={[50.5, 30.5]} radius={200} />
         </Pane>
       </MapContainer>
+
+      
     </>
   );
 };
