6b513cff68960ff89db3d32eeaee024ae0db5b10
me-loser <puspendramahariya@gmail.com>
2023-07-31 16:40:54 +0530

    dashboard error resolved

--- Frontend/src/Components/Dashboard/Profile.jsx
+++ Frontend/src/Components/Dashboard/Profile.jsx
@@ -1,16 +1,15 @@
 import classes from "../../styles/Dashboard.module.css";
 const Profile = ({ user }) => {
-  const { name, email } = user;
   return (
     <div className={classes["main-item"]}>
       <h1>Profile</h1>
       <div className={classes["profile-item"]}>
         <div className={classes["profile-item-heading"]}>Name</div>
-        <div className={classes["profile-item-content"]}>{name}</div>
+        <div className={classes["profile-item-content"]}>{user?.name}</div>
       </div>
       <div className={classes["profile-item"]}>
         <div className={classes["profile-item-heading"]}>Email</div>
-        <div className={classes["profile-item-content"]}>{email}</div>
+        <div className={classes["profile-item-content"]}>{user?.email}</div>
       </div>
     </div>
   );
