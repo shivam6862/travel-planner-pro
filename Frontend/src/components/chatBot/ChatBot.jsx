9670af6a18277422514465d9af55132304aa1c22
me-loser <puspendramahariya@gmail.com>
2023-08-01 23:51:58 +0530

    changed animations

--- Frontend/src/components/chatBot/ChatBot.jsx
+++ Frontend/src/components/chatBot/ChatBot.jsx
@@ -2,7 +2,7 @@
 import { Avatar } from "@mui/material";
 import MessageRoundedIcon from "@mui/icons-material/MessageRounded";
 import styles from "../../styles/ChatBot.module.css";
-import { useState } from "react";
+import { use, useState } from "react";
 import ChatContainer from "./ChatContainer";
 const ChatBot = () => {
   const [toggleChat, setToggleChat] = useState(false);
@@ -19,7 +19,12 @@ const ChatBot = () => {
 
   return (
     <>
-      <div className={botIconClassName} onClick={() => setToggleChat(true)}>
+      <div
+        className={botIconClassName}
+        onClick={() => {
+          setToggleChat(true);
+        }}
+      >
         <Avatar sx={{ bgcolor: "blue", width: 50, height: 50 }}>
           <MessageRoundedIcon sx={{ width: 30, height: 30 }} />
         </Avatar>
